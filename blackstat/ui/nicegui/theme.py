5dd7dedf09bdd74e55b4128e38b6aa20371bcb0a
Henrik Gregersen <support@allekankode.dk>
2025-12-21 15:23:04 +0100

    Reset theme til "standard"

--- blackstat/ui/nicegui/theme.py
+++ blackstat/ui/nicegui/theme.py
@@ -4,6 +4,7 @@ def apply_theme():
     # Use the same color palette idea as the TUI if applicable, or a modern dark theme
     ui.colors(
         primary='#5898d4',
+        #primary='#ff0000',
         secondary='#26a69a',
         accent='#ff4081',
         dark='#1d1d1d',
